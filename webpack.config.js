2be0c8ede42a6a4c858a13e56a60bff50da07886
Robert Albenda <ralbenda@gmail.com>
2022-05-29 10:08:32 -0500

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,14 +21,12 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: "babel-loader", 
         options: {
           presets: ["@babel/preset-react"],
         },
       },
     ],
   },
-  return: {
-    plugins: [new webpack.DefinePlugin(envKeys)],
-  },
+  plugins: [new webpack.DefinePlugin(envKeys)],
 };
