f4238a04c0964f13ebc45b26f4ee0701c25ce4e1
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2022-06-18 00:45:15 -0400

    nothing

--- client/components/Home.js
+++ client/components/Home.js
@@ -8,7 +8,6 @@ import CreatePost from "./CreatePost";
 import ClippedDrawer from "./SideBar";
 import Posts from "./Posts";
 
-
 const Home = () => {
   const restaurants = useSelector(({ restaurants }) => restaurants);
   const posts = useSelector(({ posts }) => posts);
@@ -23,31 +22,13 @@ const Home = () => {
   }
 
   return (
-<<<<<<< HEAD
-    <div>
-      {/* <ClippedDrawer /> */}
-      <div className="landing">
-        <Trending />
-        <ByCity />
-        <AllRestaurants />
-        <Favorites />
-        <div id="landing">
-          <h1> Welcome {auth.username}</h1>
-          <Route component={CreatePost} />
-          <h2>My Feed</h2>
-          <Route component={Posts} />
-        </div>
-=======
     <div className="landing">
       <Recommended />
       <Trending />
-     
-      
+
       <div id="landing">
         <h1> Welcome {auth.username}</h1>
->>>>>>> d36dd87f245bf61fcde0e1c9a2cd2f3f8c7608be
       </div>
-      {/* <Posts /> */}
     </div>
   );
 };
