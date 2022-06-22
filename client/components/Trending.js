147729c15d7ca4f240be552bea0461b52626406b
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2022-06-21 21:34:29 -0400

    nothing

--- client/components/Trending.js
+++ client/components/Trending.js
@@ -1,67 +1,66 @@
-
 import React from "react";
 import { useSelector } from "react-redux";
-import {Link} from "react-router-dom";
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Masonry from '@mui/lab/Masonry';
-import { styled } from '@mui/material/styles';
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Masonry from "@mui/lab/Masonry";
+import { styled } from "@mui/material/styles";
 
-const Trending= () => {
-  const {businesses: restaurants} = useSelector(({ restaurants }) => restaurants);
+const Trending = () => {
+  const { businesses: restaurants } = useSelector(
+    ({ restaurants }) => restaurants
+  );
   const auth = useSelector(({ auth }) => auth);
   const Label = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(0.5),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
   }));
-  
-      if (!restaurants) {
-          return null;
-        }
 
-          return (
-              <div>
-               
-                <h2 style={{textAlign:"center"}}> Here are LoopedIn's Top Trending Restaurants! </h2>
-                <Box sx={{ width: 800, minHeight: 829 }}>
-                            <Masonry columns={3} spacing={2}>
-                              
-                              {restaurants.map((restaurant, index) =>  (
-                                
-                                <div key={index}>
-                                  <Link to={`/trending/${restaurant.alias}`}>
-                                  <Label>{restaurant.name}</Label>
-                                  <img
-                                    src={`${restaurant.image_url}?w=162&auto=format`}
-                                    srcSet={`${restaurant.image_url}?w=162&auto=format&dpr=2 2x`}
-                                    alt={restaurant.title}
-                                    loading="lazy"
-                                    style={{
-                                      borderBottomLeftRadius: 4,
-                                      borderBottomRightRadius: 4,
-                                      display: 'block',
-                                      width: '100%',
-                                    }}
-                                  />
-                                  </Link>
-                                </div>
-                              ))}
-                            </Masonry>
-                          </Box>
+  if (!restaurants) {
+    return null;
+  }
+  console.log(restaurants);
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>
+        {" "}
+        Here are LoopedIn's Top Trending Restaurants!{" "}
+      </h2>
+      <Box sx={{ width: 800, minHeight: 829 }}>
+        <Masonry columns={3} spacing={2}>
+          {restaurants.map((restaurant, index) => (
+            <div key={index}>
+              <Link to={`/trending/${restaurant.alias}`}>
+                <Label>{restaurant.name}</Label>
+                <img
+                  src={`${restaurant.image_url}?w=162&auto=format`}
+                  srcSet={`${restaurant.image_url}?w=162&auto=format&dpr=2 2x`}
+                  alt={restaurant.title}
+                  loading="lazy"
+                  style={{
+                    borderBottomLeftRadius: 4,
+                    borderBottomRightRadius: 4,
+                    display: "block",
+                    width: "100%",
+                  }}
+                />
+              </Link>
+            </div>
+          ))}
+        </Masonry>
+      </Box>
+    </div>
+  );
+};
 
-                </div>
-              )
-          }
-        
-          export default Trending;
+export default Trending;
 
-
-          /* { <ul >
+/* { <ul >
                  {restaurants.map((restaurant,idx) => {
                    if(restaurant.rating === 5 && idx<15)
             return (
